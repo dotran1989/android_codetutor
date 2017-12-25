0e999978db5e5b2bf67e698704922748f33cc02c
dotran1989 <dotran1989@gmail.com>
2017-12-25 19:02:59 +0700

    error when updating UI on seperate thread

--- MainThread/app/src/main/java/com/example/quoccuong/mainthread/MainActivity.java
+++ MainThread/app/src/main/java/com/example/quoccuong/mainthread/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "Cuong";
 
     private Button btnStartThread, btnStopThread;
+    private TextView txtThreadCount;
 
     private boolean mStopLoop;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnStartThread = findViewById(R.id.btnStartThread);
         btnStopThread = findViewById(R.id.btnStopThread);
+        txtThreadCount = findViewById(R.id.textThreadCount);
 
         btnStartThread.setOnClickListener(this);
         btnStopThread.setOnClickListener(this);
@@ -38,7 +42,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void run() {
                         while (mStopLoop) {
+                            try {
+                                Thread.sleep(1000);
+                                count++;
+                            }catch (Exception e) {
+                                Log.i(TAG, e.getMessage());
+                            }
                             Log.i(TAG, "Thread id in while loop: " + Thread.currentThread().getId());
+                            txtThreadCount.setText(" " + count );
                         }
                     }
                 }).start();
