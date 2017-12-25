5474e5932fb961085fe5be5b04d252aef2c44807
dotran1989 <dotran1989@gmail.com>
2017-12-25 18:44:49 +0700

    creating seperate thread

--- MainThread/app/src/main/java/com/example/quoccuong/mainthread/MainActivity.java
+++ MainThread/app/src/main/java/com/example/quoccuong/mainthread/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.quoccuong.mainthread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "Cuong";
+
+    private Button btnStartThread, btnStopThread;
+
+    private boolean mStopLoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Thread id: " + Thread.currentThread().getId());
+
+        btnStartThread = findViewById(R.id.btnStartThread);
+        btnStopThread = findViewById(R.id.btnStopThread);
+
+        btnStartThread.setOnClickListener(this);
+        btnStopThread.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnStartThread:
+                mStopLoop = true;
+                // create a new Thread for loop
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (mStopLoop) {
+                            Log.i(TAG, "Thread id in while loop: " + Thread.currentThread().getId());
+                        }
+                    }
+                }).start();
+                break;
+            case R.id.btnStopThread:
+                mStopLoop = false;
+                break;
+        }
     }
 }
