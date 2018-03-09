0f76f221920783d1f4c70466948b1e19416546c6
dotran1989 <dotran1989@gmail.com>
2018-03-09 15:39:04 +0700

    start and destroy service

--- Service/app/src/main/java/expert/android/quoccuong/service/MainActivity.java
+++ Service/app/src/main/java/expert/android/quoccuong/service/MainActivity.java
@@ -1,13 +1,45 @@
 package expert.android.quoccuong.service;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnStartService;
+    private Button btnStopService;
+
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("CuongDNQ", "MainActivity, thread id: " + Thread.currentThread().getId());
+
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStopService = findViewById(R.id.btn_stop_service);
+
+        // explicit intent
+        serviceIntent = new Intent(getApplicationContext(), MyService.class);
+
+        btnStartService.setOnClickListener(this);
+        btnStopService.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start_service:
+                startService(serviceIntent);
+                break;
+            case R.id.btn_stop_service:
+                break;
+        }
     }
 }
--- Service/app/src/main/java/expert/android/quoccuong/service/MyService.java
+++ Service/app/src/main/java/expert/android/quoccuong/service/MyService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by QUOC CUONG on 09/03/2018.
@@ -20,10 +21,13 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.i("CuongDNQ", "In onDestroy, thread id: " + Thread.currentThread().getId());
     }
 
     @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("CuongDNQ", "In onStartCommand, thread id: " + Thread.currentThread().getId());
+        stopSelf();
+        return super.onStartCommand(intent, flags, startId);
     }
 }
