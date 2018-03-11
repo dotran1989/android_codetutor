e0b3d98869d8d43e2d7062f84fc612ebb2b31fd2
dotran1989 <dotran1989@gmail.com>
2018-03-11 13:59:54 +0700

    add button to add fragment

--- Fragment/app/src/main/java/expert/android/quoccuong/fragment/MainActivity.java
+++ Fragment/app/src/main/java/expert/android/quoccuong/fragment/MainActivity.java
@@ -5,15 +5,26 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAddFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("CuongDNQ", "onCreate");
         setContentView(R.layout.activity_main);
-        addFragment();
+
+        btnAddFragment = findViewById(R.id.btn_add_fragment);
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFragment();
+            }
+        });
     }
 
     @Override
