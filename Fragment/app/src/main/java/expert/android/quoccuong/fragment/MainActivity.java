be48fcb9b16249e6af4331c74dfb4eaf8f4d0fe0
dotran1989 <dotran1989@gmail.com>
2018-03-11 13:48:58 +0700

    lifecycle fragment and activity

--- Fragment/app/src/main/java/expert/android/quoccuong/fragment/MainActivity.java
+++ Fragment/app/src/main/java/expert/android/quoccuong/fragment/MainActivity.java
@@ -4,14 +4,46 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("CuongDNQ", "onCreate");
         setContentView(R.layout.activity_main);
         addFragment();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("CuongDNQ", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("CuongDNQ", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("CuongDNQ", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("CuongDNQ", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("CuongDNQ", "onDestroy");
+        super.onDestroy();
     }
 
     private void addFragment() {
--- Fragment/app/src/main/java/expert/android/quoccuong/fragment/SampleFragment.java
+++ Fragment/app/src/main/java/expert/android/quoccuong/fragment/SampleFragment.java
@@ -1,7 +1,9 @@
 package expert.android.quoccuong.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,70 @@ import android.view.ViewGroup;
 
 public class SampleFragment extends android.support.v4.app.Fragment {
 
+    @Override
+    public void onAttach(Context context) {
+        Log.i("CuongDNQ", "SampleFragment onAttach");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.i("CuongDNQ", "SampleFragment onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.i("CuongDNQ", "SampleFragment onCreateView");
         return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i("CuongDNQ", "SampleFragment onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i("CuongDNQ", "SampleFragment onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("CuongDNQ", "SampleFragment onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("CuongDNQ", "SampleFragment onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("CuongDNQ", "SampleFragment onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.i("CuongDNQ", "SampleFragment onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("CuongDNQ", "SampleFragment onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i("CuongDNQ", "SampleFragment onDetach");
+        super.onDetach();
     }
 }
