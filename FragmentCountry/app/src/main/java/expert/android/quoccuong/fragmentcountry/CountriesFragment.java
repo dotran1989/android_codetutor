b8c33937c161644864fd7dc8d8bbd123679ed56e
dotran1989 <dotran1989@gmail.com>
2018-03-16 11:45:13 +0700

    fragment countries listview

---
+++ FragmentCountry/app/src/main/java/expert/android/quoccuong/fragmentcountry/CountriesFragment.java
@@ -0,0 +1,58 @@
+package expert.android.quoccuong.fragmentcountry;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by QUOC CUONG on 16/03/2018.
+ */
+
+public class CountriesFragment extends Fragment {
+
+    View rootView;
+
+    ListView listViewCountries;
+    Context context;
+    String[] countries;
+    ArrayAdapter<String> countryNamesAdapter;
+
+    FragmentActionListener fragmentActionListener;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_countries, container, false);
+        initUI();
+        return rootView;
+    }
+
+    public void setFragmentActionListener(FragmentActionListener fragmentActionListener) {
+        this.fragmentActionListener = fragmentActionListener;
+    }
+
+    private void initUI() {
+        context = getContext();
+        countries = getResources().getStringArray(R.array.countries);
+        listViewCountries = rootView.findViewById(R.id.listViewCountries);
+
+        countryNamesAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, countries);
+        listViewCountries.setAdapter(countryNamesAdapter);
+
+        listViewCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (fragmentActionListener != null) {
+                    fragmentActionListener.onCountrySelected(countries[i]);
+                }
+            }
+        });
+    }
+}
---
+++ FragmentCountry/app/src/main/java/expert/android/quoccuong/fragmentcountry/FragmentActionListener.java
@@ -0,0 +1,12 @@
+package expert.android.quoccuong.fragmentcountry;
+
+/**
+ * Created by QUOC CUONG on 16/03/2018.
+ */
+
+public interface FragmentActionListener {
+
+    String KEY_SELECTED_COUNTRY = "KEY_SELECTED_COUNTRY";
+
+    void onCountrySelected(String country);
+}
